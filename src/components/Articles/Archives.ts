f0c93090f750056c85c9a42a758a29b26cf73ad2
Davina Leong <leong.shi.yun@gmail.com>
2025-09-26 02:34:09 +0800

    Done with the Archives article

--- src/components/Articles/Archives.ts
+++ src/components/Articles/Archives.ts
@@ -1,3 +1,5 @@
+import img0014 from "./../../assets/images/0014.png"
+import img0015 from "./../../assets/images/0015.png"
 import Menu from "./../Menu"
 import DividerArticle from "../Dividers/Article"
 
@@ -17,6 +19,31 @@ export default function ArticleRestore(): string {
             ${navMenu}
 
             <hr>
+
+            <h4 id="archives-main" class="font-serif font-semibold">Main Page</h4>
+            <p>The main Archives page lists all of your archived galleries.</p>
+            <img src="${img0014}" alt="Screenshot 0014" class="rounded-sm shadow-md">
+
+            <h4 id="archives-restore" class="font-serif font-semibold">Restoring an Archive</h4>
+            <p><strong>Steps:</strong></p>
+            <ol class="list-decimal list-inside">
+                <li>Select <strong>Archives</strong>.</li>
+                <li>Select the <strong>archive you want to restore</strong>.</li>
+                <li>Select <strong>Restore</strong>.</li>
+                <li>Select <strong>Galleries</strong>.</li>
+                <li>You should see the <strong>restored Gallery</strong>.</li>
+            </ol>
+            <img src="${img0015}" alt="Screenshot 0015" class="rounded-sm shadow-md">
+
+            <h4 id="archives-delete" class="font-serif font-semibold">Permanently Deleting an Archive</h4>
+            <p><strong>Steps:</strong></p>
+            <ol class="list-decimal list-inside">
+                <li>Select <strong>Archives</strong>.</li>
+                <li>Select the <strong>archive you want to restore</strong>.</li>
+                <li>Select <strong>Delete Permanently</strong>.</li>
+                <li>The current Gallery should be deleted and you should be <strong>redirected to Galleries</strong>.</li>
+            </ol>
+            <img src="${img0015}" alt="Screenshot 0015" class="rounded-sm shadow-md">
 
             ${DividerArticle()}
                     
